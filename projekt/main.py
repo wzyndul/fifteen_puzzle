6272c3e1247e9c51b2a92a8a8b5eb1af689e192d
wzyndul <wojteckiz8630@gmail.com>
2023-03-15 19:54:45 +0100

    added manhattan and hamming heuristic. Added generated pseudo code to A*

--- projekt/main.py
+++ projekt/main.py
@@ -15,6 +15,29 @@ COL = cols
 MAX_DEPTH = 20
 
 
+def manhattan_heuristic(board):
+    distance = 0
+    for x in range(0, ROW):
+        for y in range(0, COL):
+            board_value = board[x * ROW + y]  # wartosc z boarda
+            if board_value != 0:
+                current_x = y
+                current_y = x
+                proper_x = (board_value - 1) % COL
+                proper_y = (board_value - 1) // ROW  # floor division
+                distance += abs(proper_x - current_x) + abs(proper_y - current_y)
+    return distance  # trzeba dodac jeszcze glebokosc
+
+
+def hamming_heuristic(board):
+    distance = 0
+    for number in range(0, COL * ROW):
+        board_value = board[number]
+        if number != board_value - 1 and board_value != 0:
+            distance += 1
+    return distance  # trzeba dodac jeszcze glebokosc
+
+
 def is_solved_v2(board):
     solution = list(range(1, ROW * COL)) + [0]
     return board == tuple(solution)
@@ -95,7 +118,6 @@ def dfs_algorithm_v2(board, path, visited, current_depth, priority):
     return None
 
 
-
 def bfs_algorithm(board, priority):
     visited = set()
     q = deque([(board, "")])
@@ -135,6 +157,8 @@ def bfs_algorithm(board, priority):
     return None
 
 
+print(f"odleglosc manhattan: {manhattan_heuristic(list_puzzle)}")
+print(f"odleglosc hamminga: {hamming_heuristic(list_puzzle)}")
 algorithm_result = None
 if sys.argv[1] == "dfs":
     star_time = time.time_ns()
@@ -155,53 +179,101 @@ with open(f"{sys.argv[4]}", "w") as file:  # otwiera plik i automatycznie go zam
     else:
         file.write("-1")
 
-#TODO zrobic jakeis hashowanie do porównywania stanów w visited
+# TODO zrobic jakeis hashowanie do porównywania stanów w visited
+
+# Step-by-step explanation:
+#
+# 1. We start by initializing the open and closed lists. The open list contains only the start node, and the closed list is initially empty.
+#
+# 2.We also initialize two dictionaries: cost, which contains the cost to reach each node from the start node, and parent,
+# which contains the parent node of each node. For the start node, the cost is 0 and the parent is None.
+#
+# 3.We enter a loop that continues until the open list is empty.
+#
+# 4.In each iteration of the loop, we get the node with the lowest cost from the open list. We use a function h to estimate
+# the remaining cost from the current node to the goal node. We add the estimated cost to the cost to reach the current node to get the total cost. This is used to select the node with the lowest total cost.
+#
+# 5. If the current node is the goal node, we have found a path from the start node to the goal node. We construct the path by
+# following the parent pointers from the goal node to the start node, and then reverse the path to get the correct order. We return the path.
+#
+# 6. If the current node is not the goal node, we move it from the open list to the closed list.
+#
+# 7.We loop through the neighbors of the current node.
+#
+# 8.If a neighbor has already been evaluated (i.e., it is in the closed list), we skip it and move on to the next neighbor.
+#
+# 9.We calculate the tentative cost to reach the neighbor node by adding the cost to reach the current node to the cost
+# to move from the current node to the neighbor node.
+#
+# 10.If the neighbor is not in the open list, or if the tentative cost is lower than the current cost to reach the
+# neighbor, we update the cost and parent dictionaries for the neighbor node.
+#
+# 11.If the neighbor node is not already in the open list, we add it to the open list.
+#
+# 12.After all neighbors of the current node have been processed, we repeat the loop until the open list is empty.
+#
+# 13.If the loop completes without finding a path to the goal node, we return None to indicate that no path was found.
+#
+# Overall, the A* algorithm works by using heuristics to guide the search for the shortest path from the start node to
+# the goal node. It uses a combination of the cost to reach each node and an estimate of the remaining cost to the goal
+# node to determine which nodes to explore next. By exploring nodes with lower total cost first, it is able to find an
+# optimal path from the start node to the goal node.
 
 
-
-# STARA WERSJA PROGRAMU BEZ HASHOWANIA
-# def dfs_algorithm_v3(board, path, visited, current_depth, priority):
-#     if is_solved_v2(tuple(board)):
-#         return path
-#     if current_depth >= MAX_DEPTH:
-#         return None
-#     visited.add(tuple(board))
-#     index = board.index(0)
-#     for move in priority:
-#         if move == "U" and index >= COL:  # mozna sie ruszyc do góry
-#             if path == "" or path[-1] != "D":
-#                 new_board = change_state(board, "U", index)
-#                 if tuple(new_board) not in visited:
-#                     path += "U"  # path.append("U")
-#                     result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-#                     if result is not None:
-#                         return result
-#                     path = path[:-1]  # path.pop()
-#         elif move == "L" and index % COL != 0:  # ruch w lewo
-#             if path == "" or path[-1] != "R":
-#                 new_board = change_state(board, "L", index)
-#                 if tuple(new_board) not in visited:
-#                     path += "L"
-#                     result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-#                     if result is not None:
-#                         return result
-#                     path = path[:-1]
-#         elif move == "D" and index < COL * ROW - COL:  # ruch w dol
-#             if path == "" or path[-1] != "U":
-#                 new_board = change_state(board, "D", index)
-#                 if tuple(new_board) not in visited:
-#                     path += "D"
-#                     result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-#                     if result is not None:
-#                         return result
-#                     path = path[:-1]
-#         elif move == "R" and (index + 1) % COL != 0 and index < COL * ROW - 1:  # ruch w prawo
-#             if path == "" or path[-1] != "L":
-#                 new_board = change_state(board, "R", index)
-#                 if tuple(new_board) not in visited:
-#                     path += "R"
-#                     result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-#                     if result is not None:
-#                         return result
-#                     path = path[:-1]
+# def a_star(start, goal, heuristic_func):  pseudo code for A*
+#     """
+#     A* algorithm implementation.
+#
+#     :param start: The starting node.
+#     :param goal: The goal node.
+#     :param heuristic_func: A function that calculates the heuristic cost between two nodes.
+#     :return: The path from start to goal.
+#     """
+#     # Initialize open and closed lists
+#     open_list = [start]
+#     closed_list = []
+#
+#     # Initialize the cost and the parent dictionary
+#     cost = {start: 0}
+#     parent = {start: None}
+#
+#     # Loop until the open list is empty
+#     while open_list:
+#         # Get the node with the lowest cost
+#         current = min(open_list, key=lambda node: cost[node] + heuristic_func(node, goal))
+#
+#         # Check if the goal is reached
+#         if current == goal:
+#             path = []
+#             while current is not None:
+#                 path.append(current)
+#                 current = parent[current]
+#             path.reverse()
+#             return path
+#
+#         # Move the current node from the open list to the closed list
+#         open_list.remove(current)
+#         closed_list.append(current)
+#
+#         # Loop through the neighbors of the current node
+#         for neighbor in current.neighbors:
+#             # Check if the neighbor is already evaluated
+#             if neighbor in closed_list:
+#                 continue
+#
+#             # Calculate the tentative cost to reach the neighbor node
+#             tentative_cost = cost[current] + current.cost_to(neighbor)
+#
+#             # Check if the neighbor node is not in the open list
+#             if neighbor not in open_list:
+#                 open_list.append(neighbor)
+#             # Check if the tentative cost is greater than or equal to the cost to reach the neighbor node
+#             elif tentative_cost >= cost[neighbor]:
+#                 continue
+#
+#             # Update the cost and parent dictionary
+#             cost[neighbor] = tentative_cost
+#             parent[neighbor] = current
+#
+#     # No path found
 #     return None
