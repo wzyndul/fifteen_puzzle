1815ecaf3ef43c977281d2941caddb725f41f05f
wzyndul <wojteckiz8630@gmail.com>
2023-03-18 18:10:07 +0100

    finished a_star with both heuristic

--- projekt/board.py
+++ projekt/board.py
@@ -2,62 +2,109 @@ import copy
 
 
 class Board:
-    def __init__(self, col, row, puzzle):
+    def __init__(self, col, row, puzzle, priority):
         self.col = col
         self.row = row
         self.board = puzzle
         self.parent = None
         self.last_move = ''
+        if priority == "hamm" or priority == "manh": # jesli mam do czynienia z a starem
+            self.priority = "LURD"  # to na sztywno daje wybrana kolejnosc
+            self.heuristic = priority
+        else:
+            self.priority = priority
+            self.heuristic = None
+        self.neighbors = []
+        self.depth = 0
+
+    def get_neighbors(self):
+        return self.neighbors
 
     def __copy__(self):
         new_board = copy.deepcopy(self.board)
-        new_instance = Board(self.col, self.row, new_board)
+        new_instance = Board(self.col, self.row, new_board, self.priority)
         new_instance.last_move = self.last_move
-        new_instance.parent = self.parent
+        new_instance.parent = self
+        new_instance.priority = self.priority
+        new_instance.depth = self.depth + 1
+        new_instance.heuristic = self.heuristic
         return new_instance
 
     def __hash__(self):
         return hash(tuple(self.board))
 
-    def is_solved(self):
+    def is_solved(self): # sprawdzenie czy mamy rozwiązanie
         solution = list(range(1, self.row * self.col)) + [0]
         return tuple(self.board) == tuple(solution)
 
-    def change_state(self, board, move, index):
-        new_board = board.copy()
+    def change_state(self, move, index):
+        new_board = self.__copy__()
+        new_board.last_move = move
         if move == "U":
-            temp = new_board[index - self.col]
-            new_board[index - self.col] = 0
-            new_board[index] = temp
+            temp = new_board.board[index - self.col]
+            new_board.board[index - self.col] = 0
+            new_board.board[index] = temp
             return new_board
 
         elif move == "D":
-            temp = new_board[index + self.col]
-            new_board[index + self.col] = 0
-            new_board[index] = temp
+            temp = new_board.board[index + self.col]
+            new_board.board[index + self.col] = 0
+            new_board.board[index] = temp
             return new_board
 
         elif move == "R":
-            temp = new_board[index + 1]
-            new_board[index + 1] = 0
-            new_board[index] = temp
+            temp = new_board.board[index + 1]
+            new_board.board[index + 1] = 0
+            new_board.board[index] = temp
             return new_board
 
         elif move == "L":
-            temp = new_board[index - 1]
-            new_board[index - 1] = 0
-            new_board[index] = temp
+            temp = new_board.board[index - 1]
+            new_board.board[index - 1] = 0
+            new_board.board[index] = temp
             return new_board
         return None
 
-    def move(self, priority):
+    def move(self):
         index = self.board.index(0)
-        for move in priority:
-            if move == "U" and int(index / self.col) != 0:  # mozna sie ruszyc do góry   #index >= COL:
-                pass
-            elif move == "L" and index % self.col != 0:  # ruch w lewo index % COL != 0
-                pass
-            elif move == "D" and int(index / self.col) != self.row - 1:  # ruch w dol  index < COL * ROW - COL
-                pass
-            elif move == "R" and index % self.col != self.col - 1:  # ruch w prawo  and (index + 1) % COL != 0 and index < COL * ROW - 1
+        for move in self.priority:
+            if move == "U" and self.last_move != "D" and int(
+                    index / self.col) != 0:
+                self.neighbors.append(self.change_state(move, index))
+            elif move == "L" and self.last_move != "R" and index % self.col != 0:
+                self.neighbors.append(self.change_state(move, index))
+            elif move == "D" and self.last_move != "U" and int(
+                    index / self.col) != self.row - 1:
+                self.neighbors.append(self.change_state(move, index))
+            elif move == "R" and self.last_move != "L" and index % self.col != self.col - 1:
+                self.neighbors.append(self.change_state(move, index))
 
+    def manhattan_heuristic(self):
+        distance = 0
+        for x in range(0, self.row):
+            for y in range(0, self.col):
+                board_value = self.board[x * self.row + y]  # wartosc z boarda
+                if board_value != 0:
+                    current_x = y
+                    current_y = x
+                    proper_x = (board_value - 1) % self.col
+                    proper_y = (board_value - 1) // self.row  # floor division
+                    distance += abs(proper_x - current_x) + abs(proper_y - current_y)
+        return distance
+
+    def hamming_heuristic(self):
+        distance = 0
+        for number in range(0, self.col * self.row):
+            board_value = self.board[number]
+            if number != board_value - 1 and board_value != 0:
+                distance += 1
+        return distance
+
+    def get_heuristic_cost(self):
+        if self.heuristic == "hamm":
+            return self.hamming_heuristic()
+        else:
+            return self.manhattan_heuristic()
+
+    def __lt__(self, other):  # musiałem zdefiniować less than "<" operator, zeby móc jakoś porównywać obiektu
+        return True  # typu Board. Zwracam, true bo jak mają taki sam koszt to juz obojetnie, ktory pierwszy
--- projekt/main.py
+++ projekt/main.py
@@ -1,7 +1,11 @@
+from queue import PriorityQueue
+
 import numpy as np
 from collections import deque
 import sys
 import time
+from board import Board
+from stare_funkcje_nie_korzystac import bfs_algorithm_v2, dfs_algorithm_v2
 
 algorithm_type = sys.argv[1]
 priority = sys.argv[2]
@@ -13,230 +17,101 @@ list_puzzle = data.flatten().tolist()
 ROW = rows
 COL = cols
 MAX_DEPTH = 20
+puzzle = Board(cols, rows, list_puzzle, priority)
 
 
-def manhattan_heuristic(board):
-    distance = 0
-    for x in range(0, ROW):
-        for y in range(0, COL):
-            board_value = board[x * ROW + y]  # wartosc z boarda
-            if board_value != 0:
-                current_x = y
-                current_y = x
-                proper_x = (board_value - 1) % COL
-                proper_y = (board_value - 1) // ROW  # floor division
-                distance += abs(proper_x - current_x) + abs(proper_y - current_y)
-    return distance  # trzeba dodac jeszcze glebokosc
-
-
-def hamming_heuristic(board):
-    distance = 0
-    for number in range(0, COL * ROW):
-        board_value = board[number]
-        if number != board_value - 1 and board_value != 0:
-            distance += 1
-    return distance  # trzeba dodac jeszcze glebokosc
-
-
-def is_solved_v2(board):
-    solution = list(range(1, ROW * COL)) + [0]
-    return board == tuple(solution)
-
-
-def change_state(board, move, index):
-    new_board = board.copy()
-    if move == "U":
-        temp = new_board[index - COL]
-        new_board[index - COL] = 0
-        new_board[index] = temp
-        return new_board
-
-    elif move == "D":
-        temp = new_board[index + COL]
-        new_board[index + COL] = 0
-        new_board[index] = temp
-        return new_board
-
-    elif move == "R":
-        temp = new_board[index + 1]
-        new_board[index + 1] = 0
-        new_board[index] = temp
-        return new_board
-
-    elif move == "L":
-        temp = new_board[index - 1]
-        new_board[index - 1] = 0
-        new_board[index] = temp
-        return new_board
-    return None
-
-
-def dfs_algorithm_v2(board, path, visited, current_depth, priority):
-    if is_solved_v2(tuple(board)):
+def dfs_algorithm(board, path, visited):
+    if board.is_solved():
         return path
-    if current_depth >= MAX_DEPTH:
+    if board.depth >= MAX_DEPTH:
         return None
-    visited.add(hash(tuple(board)))
-    index = board.index(0)
-    for move in priority:
-        if move == "U" and int(index / COL) != 0:  # mozna sie ruszyc do góry   #index >= COL:
-            if path == "" or path[-1] != "D":
-                new_board = change_state(board, "U", index)
-                if hash(tuple(new_board)) not in visited:
-                    path += "U"  # path.append("U")
-                    result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-                    if result is not None:
-                        return result
-                    path = path[:-1]  # path.pop()
-        elif move == "L" and index % COL != 0:  # ruch w lewo index % COL != 0
-            if path == "" or path[-1] != "R":
-                new_board = change_state(board, "L", index)
-                if hash(tuple(new_board)) not in visited:
-                    path += "L"
-                    result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-                    if result is not None:
-                        return result
-                    path = path[:-1]
-        elif move == "D" and int(index / COL) != ROW - 1:  # ruch w dol  index < COL * ROW - COL
-            if path == "" or path[-1] != "U":
-                new_board = change_state(board, "D", index)
-                if hash(tuple(new_board)) not in visited:
-                    path += "D"
-                    result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-                    if result is not None:
-                        return result
-                    path = path[:-1]
-        elif move == "R" and index % COL != COL - 1:  # ruch w prawo  and (index + 1) % COL != 0 and index < COL * ROW - 1
-            if path == "" or path[-1] != "L":
-                new_board = change_state(board, "R", index)
-                if hash(tuple(new_board)) not in visited:
-                    path += "R"
-                    result = dfs_algorithm_v2(new_board, path, visited, current_depth + 1, priority)
-                    if result is not None:
-                        return result
-                    path = path[:-1]
+    visited.add(board.__hash__())
+    board.move()
+    for neighbor in board.get_neighbors():
+        if neighbor.__hash__() not in visited:
+            path += neighbor.last_move  # path.append("U")
+            result = dfs_algorithm(neighbor, path, visited)
+            if result is not None:
+                return result
+            path = path[:-1]  # path.pop()
     return None
 
 
-def bfs_algorithm(board, priority):  # tez sprawdac poprzedni ruch
+def bfs_algorithm(board):
     visited = set()
     q = deque([(board, "")])
-    visited.add(tuple(board))
-
+    visited.add(board.__hash__())
     while q:
         state, path = q.popleft()
-        if is_solved_v2(tuple(state)):
+        if state.is_solved():
             return path
-
-        index = state.index(0)
-        for move in priority:
-            if move == "U" and int(index / COL) != 0:  # mozna sie ruszyc do góry
-                new_board = change_state(state, "U", index)
-                if tuple(new_board) not in visited:
-                    visited.add(tuple(new_board))
-                    q.append((new_board, path + "U"))
-
-            elif move == "L" and index % COL != 0:  # ruch w lewo
-                new_board = change_state(state, "L", index)
-                if tuple(new_board) not in visited:
-                    visited.add(tuple(new_board))
-                    q.append((new_board, path + "L"))
-
-            elif move == "D" and int(index / COL) != ROW - 1:  # ruch w dol
-                new_board = change_state(state, "D", index)
-                if tuple(new_board) not in visited:
-                    visited.add(tuple(new_board))
-                    q.append((new_board, path + "D"))
-
-            elif move == "R" and index % COL != COL - 1:  # ruch w prawo
-                new_board = change_state(state, "R", index)
-                if tuple(new_board) not in visited:
-                    visited.add(tuple(new_board))
-                    q.append((new_board, path + "R"))
-
+        state.move()
+        for neighbour in state.get_neighbors():
+            if neighbour.__hash__() not in visited:
+                visited.add(neighbour.__hash__())
+                q.append((neighbour, path + neighbour.last_move))
     return None
 
 
-def get_neighbors(board, priority, last_move):  ## dodoac
-    index = board.index(0)
-    neighbors = []
-    for move in priority:
-        if move == "U" and int(index / COL) != 0 and last_move != "D":  # mozna sie ruszyc do góry
-            new_board = change_state(board, "U", index)
-            neighbors.append((new_board, move))
-        elif move == "L" and index % COL != 0 and last_move != "R":  # ruch w lewo
-            new_board = change_state(board, "L", index)
-            neighbors.append((new_board, move))
-        elif move == "D" and int(index / COL) != ROW - 1 and last_move != "U":  # ruch w dol
-            new_board = change_state(board, "D", index)
-            neighbors.append((new_board, move))
-        elif move == "R" and index % COL != COL - 1 and last_move != "L":  # ruch w prawo
-            new_board = change_state(board, "R", index)
-            neighbors.append((new_board, move))
-    return neighbors
-
-
-def a_star_algorithm(start_board, priorit):  # niestety tylko trzymam tylko node a nie potrzebne ruchy
-    open_list = [(start_board, "")]
-    closed_list = []
-    cost = {tuple(start_board): 0}  # Convert list to tuple
-    parent = {tuple(start_board): (None, "")}  # Convert list to tuple
-    while open_list:
+def a_star_algorithm(board):  # niestety tylko trzymam tylko node a nie potrzebne ruchy
+    q = PriorityQueue()
+    q.put((0, board))
+    closed_set = set()
+    while not q.empty():
         # Get the node with the lowest cost
-        current = min(open_list, key=lambda node: cost[tuple(node)] + hamming_heuristic(node))
+        current = q.get()[1]
+        closed_set.add(current.__hash__())
 
-        # Check if the goal is reached
-        if is_solved_v2(tuple(current)):
-            path = []
-            while current is not None:
-                path.append(current)
-                current = parent[tuple(current)]
-            path.reverse()
-            return path
-
-        # Move the current node from the open list to the closed list
-        open_list.remove(current)
-        closed_list.append(current)
+        if current.is_solved():
+            path = ""
+            while current.last_move != "":
+                path += current.last_move
+                current = current.parent
+            reversed_path = path[::-1]
+            return reversed_path
 
         # Loop through the neighbors of the current node
-        for neighbor in get_neighbors(current, priorit, move):
-            # Check if the neighbor is already evaluated
-            if neighbor in closed_list:
-                continue
-            # Calculate the tentative cost to reach the neighbor node
-            tentative_cost = cost[tuple(current)] + 1
+        current.move()
+        for neighbor in current.get_neighbors():
 
-            # Check if the neighbor node is not in the open list
-            if neighbor not in open_list:
-                open_list.append((neighbor)
-            # Check if the tentative cost is greater than or equal to the cost to reach the neighbor node
-            elif tentative_cost >= cost[tuple(neighbor)]:
-                continue
-            # Update the cost and parent dictionary
-            cost[tuple(neighbor)] = tentative_cost
-            parent[tuple(neighbor)] = current
+            if neighbor.__hash__() not in closed_set:
+                cost = neighbor.depth + neighbor.get_heuristic_cost()
+                q.put((cost, neighbor))
 
     # No path found
     return None
 
 
-print(f"odleglosc manhattan: {manhattan_heuristic(list_puzzle)}")
-print(f"odleglosc hamminga: {hamming_heuristic(list_puzzle)}")
-star_time = time.time_ns()
-a_star_wynik = a_star_algorithm(list_puzzle, priority)
-elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
-print(round(elapsed_time, 3))
-print(f"wynik a stara: {a_star_wynik}")
+# zosatwiłem też stare funkcje, zeby testować czasy
 algorithm_result = None
 if sys.argv[1] == "dfs":
+    # star_time = time.time_ns()
+    # algorithm_result = dfs_algorithm_v2(list_puzzle, "", set(), 0, priority)
+    # elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
+    # print(round(elapsed_time, 3))
+    # print(algorithm_result)
+
     star_time = time.time_ns()
-    algorithm_result = dfs_algorithm_v2(list_puzzle, "", set(), 0, priority)
+    algorithm_result = dfs_algorithm(puzzle, "", set())
     elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
     print(round(elapsed_time, 3))
     print(algorithm_result)
 elif sys.argv[1] == "bfs":
+    # star_time = time.time_ns()
+    # algorithm_result = bfs_algorithm_v2(list_puzzle, priority)
+    # elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
+    # print(round(elapsed_time, 3))
+    # print(algorithm_result)
+
     star_time = time.time_ns()
-    algorithm_result = bfs_algorithm(list_puzzle, priority)
+    algorithm_result = bfs_algorithm(puzzle)
+    elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
+    print(round(elapsed_time, 3))
+    print(algorithm_result)
+
+elif sys.argv[1] == "astr":
+    star_time = time.time_ns()
+    algorithm_result = a_star_algorithm(puzzle)
     elapsed_time = (time.time_ns() - star_time) / (10 ** 6)
     print(round(elapsed_time, 3))
     print(algorithm_result)
@@ -246,105 +121,3 @@ with open(f"{sys.argv[4]}", "w") as file:  # otwiera plik i automatycznie go zam
         file.write(f"{len(algorithm_result)}\n{algorithm_result}")
     else:
         file.write("-1")
-
-# TODO zamiast patrzec ostatni char ze stringa czy byl ruch to mzoesz przekazywac jako parametr
-# TODO zawsze mozna zrobic liste i dodawac sasiadow
-# TODO zrobic jakeis hashowanie do porównywania stanów w visited
-
-
-# Step-by-step explanation:
-#
-# 1. We start by initializing the open and closed lists. The open list contains only the start node, and the closed list is initially empty.
-#
-# 2.We also initialize two dictionaries: cost, which contains the cost to reach each node from the start node, and parent,
-# which contains the parent node of each node. For the start node, the cost is 0 and the parent is None.
-#
-# 3.We enter a loop that continues until the open list is empty.
-#
-# 4.In each iteration of the loop, we get the node with the lowest cost from the open list. We use a function h to estimate
-# the remaining cost from the current node to the goal node. We add the estimated cost to the cost to reach the current node to get the total cost. This is used to select the node with the lowest total cost.
-#
-# 5. If the current node is the goal node, we have found a path from the start node to the goal node. We construct the path by
-# following the parent pointers from the goal node to the start node, and then reverse the path to get the correct order. We return the path.
-#
-# 6. If the current node is not the goal node, we move it from the open list to the closed list.
-#
-# 7.We loop through the neighbors of the current node.
-#
-# 8.If a neighbor has already been evaluated (i.e., it is in the closed list), we skip it and move on to the next neighbor.
-#
-# 9.We calculate the tentative cost to reach the neighbor node by adding the cost to reach the current node to the cost
-# to move from the current node to the neighbor node.
-#
-# 10.If the neighbor is not in the open list, or if the tentative cost is lower than the current cost to reach the
-# neighbor, we update the cost and parent dictionaries for the neighbor node.
-#
-# 11.If the neighbor node is not already in the open list, we add it to the open list.
-#
-# 12.After all neighbors of the current node have been processed, we repeat the loop until the open list is empty.
-#
-# 13.If the loop completes without finding a path to the goal node, we return None to indicate that no path was found.
-#
-# Overall, the A* algorithm works by using heuristics to guide the search for the shortest path from the start node to
-# the goal node. It uses a combination of the cost to reach each node and an estimate of the remaining cost to the goal
-# node to determine which nodes to explore next. By exploring nodes with lower total cost first, it is able to find an
-# optimal path from the start node to the goal node.
-
-
-# def a_star(start, goal, heuristic_func):  pseudo code for A*
-#     """
-#     A* algorithm implementation.
-#
-#     :param start: The starting node.
-#     :param goal: The goal node.
-#     :param heuristic_func: A function that calculates the heuristic cost between two nodes.
-#     :return: The path from start to goal.
-#     """
-#     # Initialize open and closed lists
-#     open_list = [start]
-#     closed_list = []
-#
-#     # Initialize the cost and the parent dictionary
-#     cost = {start: 0}
-#     parent = {start: None}
-#
-#     # Loop until the open list is empty
-#     while open_list:
-#         # Get the node with the lowest cost
-#         current = min(open_list, key=lambda node: cost[node] + heuristic_func(node, goal))
-#
-#         # Check if the goal is reached
-#         if current == goal:
-#             path = []
-#             while current is not None:
-#                 path.append(current)
-#                 current = parent[current]
-#             path.reverse()
-#             return path
-#
-#         # Move the current node from the open list to the closed list
-#         open_list.remove(current)
-#         closed_list.append(current)
-#
-#         # Loop through the neighbors of the current node
-#         for neighbor in current.neighbors:
-#             # Check if the neighbor is already evaluated
-#             if neighbor in closed_list:
-#                 continue
-#
-#             # Calculate the tentative cost to reach the neighbor node
-#             tentative_cost = cost[current] + current.cost_to(neighbor)
-#
-#             # Check if the neighbor node is not in the open list
-#             if neighbor not in open_list:
-#                 open_list.append(neighbor)
-#             # Check if the tentative cost is greater than or equal to the cost to reach the neighbor node
-#             elif tentative_cost >= cost[neighbor]:
-#                 continue
-#
-#             # Update the cost and parent dictionary
-#             cost[neighbor] = tentative_cost
-#             parent[neighbor] = current
-#
-#     # No path found
-#     return None
